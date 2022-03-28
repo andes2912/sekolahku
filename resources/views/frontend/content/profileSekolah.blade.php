6a7c28e09f1018b0540396b590de402ceb4f1411
andes2912 <andridesmana29@gmail.com>
2022-03-29 00:09:18 +0700

    Bug Fix visi & misi dan Profile Sekolah

--- resources/views/frontend/content/profileSekolah.blade.php
+++ resources/views/frontend/content/profileSekolah.blade.php
@@ -7,11 +7,13 @@
 @section('content')
     @section('about')
         <div class="container">
-            <div style="margin-top: 5%; margin-bottom:3%">
-                <img src="{{asset('storage/images/profileSekolah/' .$profile->image)}}" class="img-responsive" style="max-height:500px; width:100%; object-fit:cover">
-            </div>
-            <h2 class="title-center">{{$profile->title}}</h2>
-            <p class="sub-title-full-width">{{$profile->content}}</p>
+            @if ($profile)
+                <div style="margin-top: 5%; margin-bottom:3%">
+                    <img src="{{asset('storage/images/profileSekolah/' .$profile->image)}}" class="img-responsive" style="max-height:500px; width:100%; object-fit:cover">
+                </div>
+                <h2 class="title-center">{{$profile->title}}</h2>
+                <p class="sub-title-full-width">{{$profile->content}}</p>
+            @endif
         </div>
     @endsection
 
--- resources/views/frontend/content/visimisi.blade.php
+++ resources/views/frontend/content/visimisi.blade.php
@@ -6,25 +6,27 @@
 @section('content')
     @section('about')
         <div class="container">
-            <div class="row about-page1-inner" style="margin-top: 5%">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <div class="about-page-content-holder">
-                        <div class="content-box">
-                            <h2>VISI</h2>
-                            <p>{{$visimisi->visi}}</p>
+            @if ($visimisi)
+                <div class="row about-page1-inner" style="margin-top: 5%">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="about-page-content-holder">
+                            <div class="content-box">
+                                <h2>VISI</h2>
+                                <p>{{$visimisi->visi}}</p>
+                            </div>
+                            <div class="content-box">
+                                <h2>MISI</h2>
+                                <p>{{$visimisi->misi}}</p>
+                            </div>
                         </div>
-                        <div class="content-box">
-                            <h2>MISI</h2>
-                            <p>{{$visimisi->misi}}</p>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div class="about-page-img-holder">
+                            <img src="{{asset('storage/images/visimisi/' .$visimisi->image)}}" class="img-responsive" alt="visimisi">
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <div class="about-page-img-holder">
-                        <img src="{{asset('storage/images/visimisi/' .$visimisi->image)}}" class="img-responsive" alt="visimisi">
-                    </div>
-                </div>
-            </div>
+            @endif
         </div>
     @endsection
 
