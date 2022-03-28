ee1f1cf865bca2c3de0c4ad42e27f8cff0f9d705
andes2912 <andridesmana29@gmail.com>
2022-03-28 23:52:56 +0700

    Profile Sekolah Frontend

---
+++ resources/views/frontend/content/profileSekolah.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.frontend.app')
+
+@section('title')
+    Profile Sekolah
+@endsection
+
+@section('content')
+    @section('about')
+        <div class="container">
+            <div style="margin-top: 5%; margin-bottom:3%">
+                <img src="{{asset('storage/images/profileSekolah/' .$profile->image)}}" class="img-responsive" style="max-height:500px; width:100%; object-fit:cover">
+            </div>
+            <h2 class="title-center">{{$profile->title}}</h2>
+            <p class="sub-title-full-width">{{$profile->content}}</p>
+        </div>
+    @endsection
+
+    {{-- Guru --}}
+    @section('guru')
+        @include('frontend.content.guru')
+    @endsection
+@endsection
