0d6a5066c43e2a7bcc5cc4fa5a9f4d3ea721f1d0
andes2912 <andridesmana29@gmail.com>
2022-05-29 19:26:02 +0700

    Improve Dashboard Perpustakaan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Events;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Modules\Perpustakaan\Entities\Book;
+use Modules\Perpustakaan\Entities\Borrowing;
+use Modules\Perpustakaan\Entities\Member;
+
 class HomeController extends Controller
 {
     /**
@@ -27,13 +31,22 @@ class HomeController extends Controller
         $role = Auth::user()->role;
 
         if (Auth::check()) {
-            if ($role == 'Admin' || $role == 'Guru' || $role == 'Murid' || $role == 'Staf' || $role == 'Perpustakaan') {
+            if ($role == 'Admin' || $role == 'Guru' || $role == 'Murid' || $role == 'Staf') {
 
               $event = Events::where('is_active','0')->first();
 
               return view('backend.website.home', compact('event'));
             } elseif($role == 'Guest' || $role == 'PPDB') {
                 return view('ppdb::backend.index');
+
+            // DASHBOARD PERPUSTAKAAN \\
+            } elseif ($role == 'Perpustakaan') {
+
+              $book = Book::sum('stock');
+              $borrow = Borrowing::whereNull('lateness')->count();
+              $member = Member::where('is_active',0)->count();
+              $members = Member::count();
+              return view('perpustakaan::index', compact('book','borrow','member','members'));
             }
         }
     }
