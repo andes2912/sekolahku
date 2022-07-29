829bef1638bd8827b6d9cd149b4f68c35f2d5afd
Andri Desmana <andridesmana29@gmail.com>
2022-07-29 14:23:12 +0700

    Add Account Name

---
+++ database/migrations/2022_07_29_072220_add_column_account_name_in_bank_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnAccountNameInBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bank_accounts', function (Blueprint $table) {
+            $table->string('account_name')->after('account_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bank_accounts', function (Blueprint $table) {
+            //
+        });
+    }
+}
